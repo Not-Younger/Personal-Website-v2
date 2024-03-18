0053f936726c731b4a43898d2cf28e8cd054a4d9
Jonathan Young <jonathanyoung7718@gmail.com>
2024-03-18 17:17:39 -0500

    light-dark mode

---
+++ app/ui/Navbar.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="flex p-5">
+      <div className="flex-none">
+        👾 Jonathan Young | Software Engineer
+      </div>
+      <div className="flex-auto">
+      </div>
+      <div className="flex-32">
+        toggle
+      </div>
+      <div className="flex-none w-64">
+        One
+      </div>
+      
+    </div>
+  )
+}
