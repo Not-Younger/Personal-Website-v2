310c5ae554e585ffdfe53ce86ac9e2b480c311e0
Jonathan Young <jonathanyoung7718@gmail.com>
2024-03-30 16:25:06 -0400

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className="min-h-screen mx-auto w-full flex flex-col bg-white dark:bg-gray-800">
         <Provider>
           <Navbar />
